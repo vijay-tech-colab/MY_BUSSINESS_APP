74073023ab1ac39d85316a7a6978b72a592f9acd
vijay kumar <vijaykumar06232@gmail.com>
2025-02-18 21:35:36 +0530

    setup cookies

--- Utils/sendToken.js
+++ Utils/sendToken.js
@@ -1,12 +1,8 @@
 const sendToken = (res, user, statusCode ,message) => {
     const token = user.getJwtToken();
     const options = {
-        expires: new Date(
-            Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
-        ),
+        expires: new Date(Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000),
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production" ? true : false,
-        sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", // Cross-origin safe
     };
     res.status(statusCode).cookie("token", token, options).json({
         success: true,
